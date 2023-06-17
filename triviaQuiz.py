87d2e4f9d958da25468f8e26ba87dd61771dbae2
Shaahin Chaichi <shaahin.chaichi@gmail.com>
2023-06-17 20:07:43 +0330

    Start

---
+++ triviaQuiz.py
@@ -0,0 +1,16 @@
+# Import modules
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+from ui import QuizUI
+import html
+
+question_bank = []
+for question in question_data:
+    question_text = html.unescape(question["question"])
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quizUI = QuizUI(quiz)
